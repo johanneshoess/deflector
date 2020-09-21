78aa8194eab6a75b059ad4cf7b6cae539f3072f1
Johannes Höß <johannes.hoess@coodoo.de>
2020-09-21 14:26:57 +0200

    no working storage

--- deflect.js
+++ deflect.js
@@ -1,1 +1,29 @@
-document.body.style.border = "5px solid red";
+console.log('START PLUGIN')
+
+
+let blocklist = {
+    dayily: ["*://*.reddit.com/*", "*://*.sueddeutsche.de/*"]
+}
+
+function setItem() {
+    console.log("OK - set");
+}
+
+function gotBlocklist(list) {
+    list.array.forEach(element => {
+        console.log(`Blocklist: ${element}`);
+    });
+}
+
+function onError(error) {
+    console.log(error)
+}
+
+// store the objects
+browser.storage.local.set({ blocklist })
+    .then(setItem, onError);
+
+
+
+let settingItem = browser.storage.local.get("blocklist")
+    .then(gotBlocklist, onError);
